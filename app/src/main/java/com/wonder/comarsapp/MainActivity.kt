60f03a72da3e032c1a6706cac0608c8bb7f1b276
Braian Ledantes <braianledantes@outlook.com>
2021-11-11 22:45:45 -0300

    listo andaaa,casi

--- app/src/main/java/com/wonder/comarsapp/MainActivity.kt
+++ app/src/main/java/com/wonder/comarsapp/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.wonder.comarsapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
 import com.wonder.comarsapp.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -19,13 +24,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnQRBascula.setOnClickListener {
-            obtenerSector()
+            initScanner()
         }
     }
 
-    private fun obtenerSector() {
+    private fun obtenerSector(nroRemito: Int) {
+        binding.progressBar.visibility = View.VISIBLE
         CoroutineScope(Dispatchers.IO).launch {
-            val call = getRetrofit().create(APIService::class.java).getRemito("88888")
+            Log.d("MainActivity", "haciendo consulta a: $nroRemito")
+            val call = getRetrofit().create(APIService::class.java).getRemito(nroRemito.toString())
             val remito = call.body()
 
             runOnUiThread {
@@ -35,13 +42,48 @@ class MainActivity : AppCompatActivity() {
                 } else {
                     showError("algo")
                 }
+                binding.progressBar.visibility = View.GONE
             }
         }
     }
 
+    private fun initScanner(){
+        // para que limpie la pantalla
+        showResult(null)
+
+        val integrator = IntentIntegrator(this)
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+        integrator.setPrompt("Escanea el QR para obtener el sector.")
+        // integrator.setTorchEnabled(true)
+        integrator.setBeepEnabled(true)
+        integrator.initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Cancelado", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "El valor escaneado es: " + result.contents, Toast.LENGTH_LONG).show()
+                binding.tvResult.text = result.contents
+                try {
+                    obtenerSector(result.contents.toInt())
+                } catch (e: Exception) {
+                    showError("no es un entero: ${result.contents}")
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
     private fun showResult(remito: RemitoResponse?) {
-        remito?.let {
-            binding.tvResult.text = remito.nroRemito.toString()
+        if (remito != null) {
+            binding.tvResult.text = "{\n${remito.puntoVenta},\n${remito.nroRemito},\n${remito.sector},\n" +
+                    "${remito.descargado}\n}"
+        } else {
+            binding.tvResult.text = ""
         }
     }
 
--- app/src/main/java/com/wonder/comarsapp/RemitoResponse.kt
+++ app/src/main/java/com/wonder/comarsapp/RemitoResponse.kt
@@ -3,5 +3,8 @@ package com.wonder.comarsapp
 import com.google.gson.annotations.SerializedName
 
 data class RemitoResponse(
-    @SerializedName("nroRemito") val nroRemito: Int
+    @SerializedName("puntoVenta") val puntoVenta: Int,
+    @SerializedName("nroRemito") val nroRemito: Int,
+    @SerializedName("sector") val sector: String,
+    @SerializedName("descargado") val descargado: Int
 )
